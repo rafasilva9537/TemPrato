f881c8abf04a776a97cd2eabb44f692f422fb8b6
Rafael <rafasilva9537@gmail.com>
2025-04-28 16:46:34 -0300

    Add recipes controller

---
+++ backend/RecipesApi/Controllers/RecipesController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesApi.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class RecipesController : ControllerBase
+{
+    private readonly ILogger<RecipesController> _logger;
+
+    public RecipesController(ILogger<RecipesController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult GetRecipes()
+    {
+        return Ok("Get recipes page");
+    }
+}
