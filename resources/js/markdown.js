d81eef3bcf876a3fd2266ca5720c021374bedb47
MayColdHeart <devmayy@gmail.com>
2025-05-06 20:26:46 -0300

    Add "undo" and "redo" command.

--- resources/js/markdown.js
+++ resources/js/markdown.js
@@ -2,7 +2,13 @@ function executeCommand(command, value){
     document.execCommand(command,false,value);
 }
 
+document.getElementById("undo").addEventListener("click", function(){
+    executeCommand("undo");
+});
 
+document.getElementById("redo").addEventListener("click", function(){
+    executeCommand("redo");
+});
 
 document.getElementById("bold").addEventListener("click", function() {
     executeCommand("bold");
