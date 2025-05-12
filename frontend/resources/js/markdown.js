7be409fc1881abbf52da5c99a19db82c22cea605
MayColdHeart <devmayy@gmail.com>
2025-05-12 01:14:31 -0300

    fix content

--- frontend/resources/js/markdown.js
+++ frontend/resources/js/markdown.js
@@ -1,16 +1,92 @@
-function executeCommand(command, value){
-    document.execCommand(command,false,value);
+
+
+function heading(level) {
+    document.execCommand('formatBlock', false, level);
 }
 
-document.getElementById("undo").addEventListener("click", function(){
-    executeCommand("undo");
-});
+function undo() {
+    document.execCommand('undo');
+}
 
-document.getElementById("redo").addEventListener("click", function(){
-    executeCommand("redo");
-});
+function redo() {
+    document.execCommand('redo');
+}
 
-document.getElementById("bold").addEventListener("click", function() {
-    executeCommand("bold");
-});
+function bold() {
+    document.execCommand('bold');
+}
 
+function italic() {
+    document.execCommand('italic');
+}
+
+function underline() {
+    document.execCommand('underline');
+}
+
+function strikethrough() {
+    document.execCommand('strikeThrough');
+}
+
+function subscript() {
+    document.execCommand('subscript');
+}
+
+function superscript() {
+    document.execCommand('superscript');
+}
+
+function fontSize(size) {
+    document.execCommand('fontSize', false, size);
+}
+
+function changeTextColor() {
+    let color = document.getElementById('colorTextPick').value;
+    document.execCommand('foreColor', false, color);
+}
+
+function changeBgColor() {
+    let color = document.getElementById('bgColorPick').value;
+    document.execCommand('hiliteColor', false, color);
+}
+
+function changeFont(font) {
+    document.execCommand('fontName', false, font);
+}
+
+function alignLeft() {
+    document.execCommand('justifyLeft');
+}
+
+function alignCenter() {
+    document.execCommand('justifyCenter');
+}
+
+function alignRight() {
+    document.execCommand('justifyRight');
+}
+
+function alignJustify() {
+    document.execCommand('justifyFull');
+}
+
+function outdent() {
+    document.execCommand('outdent');
+}
+
+function indent() {
+    document.execCommand('indent');
+}
+
+function orderedList() {
+    document.execCommand('insertOrderedList');
+}
+
+function unorderedList() {
+    document.execCommand('insertUnorderedList');
+}
+
+function insertImageUrl() {
+    let url = prompt("Insira a URL da imagem:");
+    if (url) document.execCommand('insertImage', false, url);
+}
